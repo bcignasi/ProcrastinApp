93f5f27302120e831b51bd133725b907a84ed728
ibuendiac <ignasi.buendia@gmail.com>
2025-10-15 19:23:55 +0200

    update new chatGPT version

--- app/src/main/java/com/ibc/procrastinapp/data/ai/RequestResponse.kt
+++ app/src/main/java/com/ibc/procrastinapp/data/ai/RequestResponse.kt
@@ -20,7 +20,7 @@ package com.ibc.procrastinapp.data.ai
  * @property messages Lista de mensajes de la conversación.
  */
 data class ChatRequest(
-    val model: String = "gpt-3.5-turbo",
+    val model: String = "gpt-4o-mini",
     val messages: List<Message>
 )
 
